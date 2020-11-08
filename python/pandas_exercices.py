eb8a8b0984a298b47ab3c3fcb0e26070091404b4
Axel Mendoza <axel.mendoza@epita.fr>
2020-11-08 15:59:52 +0100

    add first two exs

--- python/pandas_exercices.py
+++ python/pandas_exercices.py
@@ -1,12 +1,19 @@
 import pandas as pd
+import numpy as np
 
 
-# How to import pandas and check the version?
+# 1. How to import pandas and check the version?
 def import_pandas():
     import pandas as pd
     print(pd.__version__)
 
+# 2. How to create a series from a list, numpy array and dict?
+def obj_to_series(obj):
+    return pd.Series(obj)
+    
 
 if __name__ == "__main__":
-    import_pandas()
-
+    array = np.zeros(10)
+    lst = [i for i in range(10)]
+    print(obj_to_series(array))
+    print(obj_to_series(lst))
