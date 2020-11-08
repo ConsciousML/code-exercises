095d8d6e59c245a761aeaeeab687aed488a9717f
Axel Mendoza <axel.mendoza@epita.fr>
2020-11-08 16:15:42 +0100

    add serie_to_df

--- python/pandas_exercices.py
+++ python/pandas_exercices.py
@@ -11,13 +11,16 @@ def import_pandas():
 def obj_to_series(obj):
     return pd.Series(obj)
 
-# 3. How to convert the index of a series into a column of a dataframe?
+# 3. How to convert series into a dataframe?
+def serie_to_df(serie):
+    return serie.to_frame()
 
 # Serie: a one dimensional object with axis label.
 # DataFrame: two dimensional array, data structure containing labeled axes (rows and columns).
+# def series_to_column(serie, df):
+
+
 
 if __name__ == "__main__":
-    array = np.zeros(10)
-    lst = [i for i in range(10)]
-    print(obj_to_series(array))
-    print(obj_to_series(lst))
+    serie = pd.Series([i for i in range(10)])
+    print(serie.to_frame().reset_index())
