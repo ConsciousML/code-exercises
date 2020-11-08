e0e1fb4503607dbcec317d2dfeef99a4720361c5
Axel Mendoza <axel.mendoza@epita.fr>
2020-11-08 15:47:23 +0100

    start pandas exercices

---
+++ python/pandas_exercices.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+# How to import pandas and check the version?
+def import_pandas():
+    import pandas as pd
+    print(pd.__version__)
+
+
+if __name__ == "__main__":
+    import_pandas()
+
